05890f2edf0d5442f49fb0bea4623f5f98ca438e
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-11-15 00:57:35 -0600

    Finished.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
 
-    private static int ackermann(int m, int n){
-        if (m == 0){
-            return n+1;
-        }
-        else if (n==0){
-            return ackermann(m-1, 1);
-        }
-        else{
-            return ackermann(m-1, ackermann(m, n-1));
+    private static int ackermann(int m, int n) {
+        if (m == 0) {
+            return n + 1;
+        } else if (n == 0) {
+            return ackermann(m - 1, 1);
+        } else {
+            return ackermann(m - 1, ackermann(m, n - 1));
         }
     }
 
